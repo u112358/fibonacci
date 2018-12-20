033dd2d28e5d1c5e75967989a8a06d7e083d6173
u112358 <bingzhanghu1992@gmail.com>
2018-12-20 21:55:38 +0000

    hs.tree

--- hsiw.py
+++ hsiw.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os
+os.system('cls')
 gt = [77,101,114,114,121,32,67,104,114,105,115,116,109,97,115]
 text = np.random.rand(15)*120
 while np.sum(np.square(text-gt))>1e-5:
