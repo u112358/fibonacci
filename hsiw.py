9414edf2994b640e9eb74cc3ccebf97c1ce62d04
u112358 <bingzhanghu1992@gmail.com>
2018-12-20 21:52:24 +0000

    hs.tree

---
+++ hsiw.py
@@ -0,0 +1,6 @@
+import numpy as np
+gt = [77,101,114,114,121,32,67,104,114,105,115,116,109,97,115]
+text = np.random.rand(15)*120
+while np.sum(np.square(text-gt))>1e-5:
+    text -= 1e-2*(text-gt)
+print(''.join(chr(int(i)) for i in np.round(text)))
